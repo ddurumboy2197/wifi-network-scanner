1dccc61ceba1cb7d634086c3098e219144a052d5
ddurumboy2197 <ddurumboy2197@gmail.com>
2026-04-21 13:56:48 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,16 @@
+import hashlib
+import getpass
+
+def password_hashing():
+    print("Password Hashing Tool")
+    print("---------------------")
+
+    password = getpass.getpass("Enter your password: ")
+    password = password.encode('utf-8')
+
+    hash_object = hashlib.sha256(password)
+    hashed_password = hash_object.hexdigest()
+
+    print("Hashed Password: ", hashed_password)
+
+password_hashing()
